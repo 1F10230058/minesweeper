f3a6509b18dcb2f2b69a80340925963634c2e568
1F10230058 <s1f102300582@iniad.org>
2023-06-22 14:54:09 +0900

    マージ

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,7 +31,11 @@ const Home = () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0],
   ]);
-  const [board, setBoard] = useState([
+  const newUserInputs: (0 | 1 | 2 | 3)[][] = JSON.parse(JSON.stringify(userInputs));
+
+  const board = [
+    // [-1, -1, -1, -1, -1, -1, -1, -1, -1],
+    [-1, 0, 1, 2, 3, 4, 5, 6, 7],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
@@ -40,8 +44,14 @@ const Home = () => {
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
     [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-    [-1, -1, -1, -1, -1, -1, -1, -1, -1],
-  ]);
+  ];
+
+  // -1 ->石
+  // 0 ->画像なしセル
+  // 1~8 ->数字セル
+  // 9 ->石＋はてな
+  // 10 ->石＋旗
+  // 11 ->ボムセル
   const direction = [
     [-1, 0],
     [-1, 1],
@@ -51,64 +61,72 @@ const Home = () => {
     [1, -1],
     [-1, -1],
     [0, -1],
-    // -1 ->石
-    // 0 ->画像なしセル
-    // 1~8 ->数字セル
-    // 9 ->石＋はてな
-    // 10 ->石＋旗
-    // 11 ->ボムセル
   ];
+
+
+  const bombCount = 10;
+
   const isPlaying = userInputs.some((row) => row.some((input) => input !== 0));
   const isFailure = userInputs.some((row, y) =>
     row.some((input, x) => input === 1 && bombMap[y][x] === 1)
   );
   const onClick = (x: number, y: number) => {
-    console.log(x, y);
-    const notBomb = [y][x];
-    const newBombmap: number[][] = JSON.parse(JSON.stringify(bombMap));
-    function placeBombs(bombCount: 10) {
-      const rows = 9;
-      const cols = 9;
-      let bombPlaced = 0;
-      if (bombMap.flat().filter(Boolean).length === 0)
-        while (bombPlaced < bombCount) {
-          const randomRow = Math.floor(Math.random() * rows);
-          const randomCol = Math.floor(Math.random() * cols);
-          if (newBombmap[randomRow][randomCol] !== 1 && [randomRow][randomCol] !== [y][x]) {
-            newBombmap[randomRow][randomCol] = 1;
-            bombPlaced++;
+    console.log("a");
+    function placeBombs(bombCount: number) {
+      console.log(1);
+      const bombPlaced = 0;
+      const newBombMap: (0 | 1)[][] = JSON.parse(JSON.stringify(bombMap));
+      if (newBombMap.flat().filter(Boolean).length === 0) {
+        console.log(2);
+        while (newBombMap.flat().filter(Boolean).length < 10) {
+          console.log(3);
+          const randomRow = Math.floor(Math.random() * 9);
+          const randomCol = Math.floor(Math.random() * 9);
+          if (newBombMap[randomRow][randomCol] !== 1 && `${randomRow}${randomCol}` !== `${y}${x}`) {
+            console.log(4);
+            console.log(randomRow,randomCol);
+            newBombMap[randomRow][randomCol] = 1;
+            // bombPlaced++;
           }
         }
-      console.table(newBombmap);
-      return newBombmap;
-    }
-    const bombCount = 10;
-    const updatedBombMap = placeBombs(bombCount);
-    setBombMap(updatedBombMap);
-    const newUserInputs: number[][] = JSON.parse(JSON.stringify(userInputs));
-    let bombCounter = 0;
-    if (userInputs[y][x] === 0)
-      for (const [dy, dx] of direction) {
-        if (
-          board[y + dy] !== undefined &&
-          board[y + dy][x + dx] !== undefined &&
-          [y][x] === bombMap[y][x]
-        ) {
-          if (bombMap[y + dy][x + dx] === 0) bombCounter++;
-        }
-        const bombCounter1 = bombCounter;
-
-        return;
       }
+      console.table(newBombMap);
+      return newBombMap;
+    }
+    console.log(5);
+    if (userInputs[y][x] === 0) {
+      console.log(6);
+      newUserInputs[y][x] = 1;
+      setUserInputs(newUserInputs);
+    }
+    const updatedBombMap = placeBombs(10);
+    setBombMap(updatedBombMap);
   };
-
+  // const bombCounter = 0;
+  // if (userInputs[y][x] === 0) {
+  //   for (const [dy, dx] of direction) {
+  //     if (
+  //       board[y + dy] !== undefined &&
+  //       board[y + dy][x + dx] !== undefined &&
+  //       [y][x] === bombMap[y][x]
+  //     ) {
+  //       if (bombMap[y + dy][x + dx] === 0) bombCounter++;
+  //     }
+  //   }
+  // }
+  
   return (
     <div className={styles.container}>
       <div className={styles.board}>
-        {board.map((row, y) =>
-          row.map((color, x) => (
-            <div className={styles.cell} key={`${x}-${y}`} onClick={() => onClick(x, y)}>
-              {color !== 0 && <div className={styles.stone} />}
+        {newUserInputs.map((row, y) =>
+          row.map((state, x) => (
+            <div
+              className={styles.sprite}
+              key={`${x}-${y}`}
+              style={{ backgroundPosition: state * -30 + 30 }}
+              onClick={() => onClick(x, y)}
+            >
+              {state === -1 && <div className={styles.stone} />}
             </div>
           ))
         )}
